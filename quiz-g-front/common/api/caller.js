cb2c4a10606cb8f8711bab4948a3cf44c4b96813
duydd25031999 <45323184+DoDucDuyVietnam@users.noreply.github.com>
2019-12-30 14:33:21 +0700

    api builder

---
+++ quiz-g-front/common/api/caller.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const baseURL = "http://127.0.0.1:8888/api"
+const timeoutDefault = 60000
+
+/**
+ * 
+ * @param {Object} config
+ * @param {String} config.token
+ * @param {Object} config.customHeader
+ */
+function ApiCaller(
+  {token, customHeader} = {
+    token: '',
+    customHeader: false
+  }
+) {
+  let headers =  {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token}`,
+    "Access-Control-Allow-Origin": "*"
+  }
+
+  if(customHeader) {
+    headers = customHeader
+  }
+
+  const caller = axios.create({
+    baseURL,
+    headers,
+    timeout: timeoutDefault
+  });
+  return caller;
+}
+
+export default ApiCaller
